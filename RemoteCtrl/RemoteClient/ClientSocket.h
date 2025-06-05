acb6e30300ceffcce8d55d5dcceedaa575fe6256
1193540374@qq.com <1193540374@qq.com>
2025-06-05 20:44:05 +0800

    临时保存我的改动

--- RemoteCtrl/RemoteClient/ClientSocket.h
+++ RemoteCtrl/RemoteClient/ClientSocket.h
@@ -135,15 +135,15 @@ public:
 		}
 		return m_instance;
 	}
-	bool InitSocket(const std::string& strIPAddress) {
+	bool InitSocket(int nIP, int nPort) {
 		if (m_sock != INVALID_SOCKET) CloseSocket();
 		m_sock = socket(PF_INET, SOCK_STREAM, 0);
 		if (m_sock == -1)return false;
 		sockaddr_in serv_adr;
 		memset(&serv_adr, 0, sizeof(serv_adr));
 		serv_adr.sin_family = AF_INET;
-		serv_adr.sin_addr.s_addr = inet_addr(strIPAddress.c_str());
-		serv_adr.sin_port = htons(9527);
+		serv_adr.sin_addr.s_addr = htonl(nIP);
+		serv_adr.sin_port = htons(nPort);
 		if (serv_adr.sin_addr.s_addr == INADDR_NONE) {
 			AfxMessageBox("指定的ip地址不存在！！！");
 			return false;
--- RemoteCtrl/RemoteClient/Resource.h
+++ RemoteCtrl/RemoteClient/Resource.h
@@ -7,8 +7,10 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_REMOTECLIENT_DIALOG         102
 #define IDR_MAINFRAME                   128
-#define IDC_BUTTON1                     1000
 #define IDC_BTN_TEXT                    1000
+#define IDC_EDIT_PORT                   1001
+#define IDC_IPADDRESS1                  1002
+#define IDC_IPADDRESS_SERV              1002
 
 // Next default values for new objects
 // 
@@ -16,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
