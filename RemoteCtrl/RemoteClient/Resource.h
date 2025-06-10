56267cb20ea54d82a3896d69804e351eb07d738f
1193540374@qq.com <1193540374@qq.com>
2025-06-10 11:27:34 +0800

    1 完善了树控件的内容，只显示文件夹，单击也可以起效 2 完成了列表的数据显示 3 给列表追加右键菜单

--- RemoteCtrl/RemoteClient/Resource.h
+++ RemoteCtrl/RemoteClient/Resource.h
@@ -7,21 +7,29 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_REMOTECLIENT_DIALOG         102
 #define IDR_MAINFRAME                   128
+#define IDR_MENU_CLICK                  131
+#define IDR_MENU_RCLICK                 131
 #define IDC_BTN_TEXT                    1000
 #define IDC_EDIT_PORT                   1001
 #define IDC_IPADDRESS1                  1002
 #define IDC_IPADDRESS_SERV              1002
 #define IDC_TREE_DIR                    1003
 #define IDC_LIST_FILE                   1004
-#define IDC_BUTTON1                     1005
 #define IDC_BTN_FILEINFO                1005
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_DOWNLOAD_FILE                32778
+#define ID_DELETE_FILE                  32779
+#define ID_OPEN_FILE                    32780
+#define ID_RUN_FILE                     32781
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32782
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
